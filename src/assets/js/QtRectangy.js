5713a876fea5711efb3ed3218138f978f4d3c0a7
taquy-finos <quyt@fsoft.com.vn>
2019-05-10 00:23:11 +0700

    move target now success

--- src/assets/js/QtRectangy.js
+++ src/assets/js/QtRectangy.js
@@ -18,23 +18,33 @@ class Maneuver {
     constructor(master) {
         this.master = master;
         this.mouse = new MouseRecorder();
+        this.target = null;
+        // offset size to re-center the mouse in object
+        this.cpo = [];
     }
 
     // load for first time
     load() {
         let vm = this;
-        this.master.omc.containe.mousedown(function (e) {
+        this.master.omc.container.mousedown(function (e) {
             vm.mouse.down = true;
             vm.mouse.lx = e.pageX;
             vm.mouse.ly = e.pageY;
         });
 
-        this.master.omc.container.mouseup(function (e) {
+        this.master.omc.container.mousemove(function (e) {
             vm.mouse.x = e.pageX;
             vm.mouse.y = e.pageY;
+            if (vm.target) {
+                vm.target.css({
+                    left: e.pageX - vm.cpo[0],
+                    top: e.pageY - vm.cpo[1]
+                });
+            }
         });
         this.master.omc.container.mouseup(function (e) {
             vm.mouse.down = false;
+            vm.target = null;
             vm.mouse.recur();
         })
     }
@@ -100,13 +110,21 @@ class Cube {
         let cube = $("<div>");
         cube.addClass('cube');
 
-        let cpx = cp[0] - this.size.w / 2;
-        let cpy = cp[1] - this.size.h / 2;
+        let cox = this.size.w / 2;
+        let coy = this.size.h / 2;
 
         cube.css({
-            top: cpx, left: cpy,
+            top: cp[0] - cox, left: cp[1] - coy,
             width: this.size.w,
             height: this.size.h
+        });
+
+        // attach event of cube
+        cube.mousedown(e => {
+            this.master.mnu.target = cube;
+            this.master.mnu.cpo = [cox, coy];
+            this.master.mnu.mouse.lx = e.pageX;
+            this.master.mnu.mouse.ly = e.pageY;
         });
 
         return cube;
